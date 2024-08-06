a02be4f2d3be37d225a5db8fa7341ab8dca6a2d2
prasanth-krp <prasanthkrofficial@gmail.com>
2024-08-06 23:46:24 +0530

    fixed the problem of work experience tab where the start year and end year where not being displayed when came back from the adjacent tabs

--- src/components/HomePage/Home-Js-Files/DetailsFillingPage/WorkExperience.js
+++ src/components/HomePage/Home-Js-Files/DetailsFillingPage/WorkExperience.js
@@ -7,40 +7,56 @@ import { setWorkExperience } from '../../../../Redux/action';
 const WorkExperienceForm = () => {
   const dispatch = useDispatch();
   const workExperience = useSelector((state) => state.userDetails?.workExperience || []);
-  const { control, register, formState: { errors } } = useFormContext();
+  const { control, register, formState: { errors }, reset, setValue } = useFormContext();
   const { fields, append, remove } = useFieldArray({
     control,
     name: 'experiences',
   });
 
+  // Watch form data
   const formData = useWatch({ control, name: 'experiences' });
 
+  // Initialize form with values from Redux store
   useEffect(() => {
+    console.log('WorkExperience from Redux:', workExperience); // Debugging
+    if (workExperience && workExperience.length > 0) {
+      reset({ experiences: workExperience });
+    }
+  }, [workExperience, reset]);
+
+  // Dispatch form data to Redux store on change
+  useEffect(() => {
+    console.log('FormData:', formData); // Debugging
     dispatch(setWorkExperience(formData));
   }, [formData, dispatch]);
 
+  // Append initial empty fields if none exist
   useEffect(() => {
     if (fields.length === 0) {
       append({ jobTitle: '', organizationName: '', startYear: '', endYear: '' });
     }
   }, [fields, append]);
 
+  // Add new experience handler
   const handleAddExperience = () => {
     append({ jobTitle: '', organizationName: '', startYear: '', endYear: '' });
   };
 
+  // Remove experience handler
   const handleRemoveExperience = (index) => {
     if (fields.length > 1) {
       remove(index);
     }
   };
 
+  // Generate years for selection
   const getYears = useMemo(() => {
     const startYear = 2000;
     const endYear = new Date().getFullYear();
     return Array.from({ length: endYear - startYear + 1 }, (_, i) => startYear + i);
   }, []);
 
+  // Validate end year
   const validateEndYear = (startYear, endYear) => {
     if (startYear && endYear && endYear <= startYear) {
       return 'End Year must be after Start Year';
@@ -82,6 +98,7 @@ const WorkExperienceForm = () => {
               select
               {...register(`experiences.${index}.startYear`, { required: 'Start Year is required' })}
               fullWidth
+              defaultValue={field.startYear} // Default value set
               error={!!errors.experiences?.[index]?.startYear}
               helperText={errors.experiences?.[index]?.startYear?.message}
             >
@@ -101,6 +118,7 @@ const WorkExperienceForm = () => {
                 validate: (value) => validateEndYear(formData[index]?.startYear, value)
               })}
               fullWidth
+              defaultValue={field.endYear} // Default value set
               error={!!errors.experiences?.[index]?.endYear}
               helperText={errors.experiences?.[index]?.endYear?.message}
             >
